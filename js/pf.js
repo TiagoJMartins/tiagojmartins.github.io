94d4f6098c074ff77265661a2f1a2b8ce8776b12
Tiago Martins <tiagodksp@gmail.com>
2016-03-03 18:20:51 +0000

    Modify and improve basic portfolio layout, add smooth scroll and button.

--- js/pf.js
+++ js/pf.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-   
+$(document).ready(function() {
+    
+    $('a#toContent').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            'scrollTop': $('#skills').offset().top
+        }, 700, 'swing');
+    });
+    
 });
