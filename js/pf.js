67f92538733e65b2ab7badc706932a56d3a8ae4d
Tiago Martins <tiagodksp@gmail.com>
2016-03-03 18:35:54 +0000

    Add background image to intro section

--- js/pf.js
+++ js/pf.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
     
+    
+    // Button scroll to content
+    
     $('a#toContent').click(function(e) {
         e.preventDefault();
         $('html, body').animate({
             'scrollTop': $('#skills').offset().top
         }, 700, 'swing');
     });
+    
+    
+    // Triangle pattern generator
+    
+    
     
 });
